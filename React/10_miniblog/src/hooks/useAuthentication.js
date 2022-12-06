9927f5524fa2ca2586fda98c57f6ddea2b8ece40
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-12-06 16:36:55 -0300

    Adicionada Funcionalidades de Autenticação e Login

--- React/10_miniblog/src/hooks/useAuthentication.js
+++ React/10_miniblog/src/hooks/useAuthentication.js
@@ -25,6 +25,7 @@ export const useAuthentication = () => {
     }
   }
 
+  // register
   const createUser = async (data) => {
     checkIfIsCancelled();
 
@@ -52,14 +53,47 @@ export const useAuthentication = () => {
       let systemErrorMessage;
 
       if (error.message.includes("Password")) {
-        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres";
+        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
       } else if (error.message.includes("email-already")) {
-        systemErrorMessage = "Email já cadastrado";
+        systemErrorMessage = "Email já cadastrado.";
       } else {
-        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde";
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
       }
       setLoading(false);
       setError(systemErrorMessage);
+    }
+  };
+
+  // logout - sign out
+  const logout = () => {
+    checkIfIsCancelled();
+
+    signOut(auth);
+  };
+
+  // login = sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
     }
   };
 
@@ -72,5 +106,7 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
+    logout,
+    login,
   };
 };
