11f8c549863c35eb29d1b2b53d8303f12bbdac80
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-12-13 17:33:25 -0300

    UseState e input

--- React/11_hooks/src/hooks/HookUseState.js
+++ React/11_hooks/src/hooks/HookUseState.js
@@ -1,25 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const HookUseState = () => {
-    // 1 - useState
-    let userName = "João"
+  // 1 - useState
+  let userName = "João";
 
-    const [name, setName] = useState("Matheus")
+  const [name, setName] = useState("Matheus");
 
-    const changeNames = () => {
-        userName = "João Souza"
-        setName("Matheus Batisti")
-    }
+  const changeNames = () => {
+    userName = "João Souza";
+    setName("Matheus Batisti");
+  };
+
+  // 2 - useState e input
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    //envio a uma API
+    console.log(age)
+  }
 
   return (
     <div>
-        {/* 1 useState */}
-        <h2>useState</h2>
-        <p>Variável: {userName}</p>
-        <p>useState: {name}</p>
-        <button onClick={changeNames}>Mudar nomes</button>
+      {/* 1 useState */}
+      <h2>useState</h2>
+      <p>Variável: {userName}</p>
+      <p>useState: {name}</p>
+      <button onClick={changeNames}>Mudar nomes</button>
+      <hr />
+      {/* 2 useState e Input*/}
+      <p>Digite a sua idade</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="enviar " />
+      </form>
+      <p>Você tem {age} anos</p>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default HookUseState
+export default HookUseState;
