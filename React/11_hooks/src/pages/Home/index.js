d33576975f6b52d1a8f8446e3ad13318c094f84f
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-12-13 17:41:36 -0300

    use Reducer

--- React/11_hooks/src/pages/Home/index.js
+++ React/11_hooks/src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import HookUseReducer from '../../hooks/HookUseReducer'
 import HookUseState from '../../hooks/HookUseState'
 
 const Home = () => {
   return (
     <div>
       <HookUseState />
+      <HookUseReducer />
     </div>
   )
 }
