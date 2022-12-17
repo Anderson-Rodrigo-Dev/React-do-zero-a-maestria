32585de770b533b32172dde274ee0c17bcd9864b
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-12-17 15:02:30 -0300

    UseEffect com dependencia vazia

--- React/11_hooks/src/hooks/HookUseEffect.js
+++ React/11_hooks/src/hooks/HookUseEffect.js
@@ -4,13 +4,19 @@ const HookUseEffect = () => {
     // 1 - useEffect sem dependencias
     useEffect(() => {
         console.log("Estou sendo executado")
-    }, [])
+    })
 
     const [number, setNumber] = useState(1)
     
     const changeSomething = () => {
         setNumber(number+1)
     }
+
+    // 2 - array de deps. vazio
+
+    useEffect(() => {
+      console.log("Serei executado apenas 1x")
+    }, [])
 
 
   return (
