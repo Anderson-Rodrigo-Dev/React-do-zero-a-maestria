c8329332aa635860d3818bf338a602f0a2417f29
Anderson-Rodrigo-Dev <andersonrodrigodev@gmail.com>
2022-12-17 16:01:37 -0300

    useRef

--- React/11_hooks/src/pages/Home/index.js
+++ React/11_hooks/src/pages/Home/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import HookUseEffect from '../../components/hooks/HookUseEffect'
-import HookUseReducer from '../../components/hooks/HookUseReducer'
-import HookUseState from '../../components/hooks/HookUseState'
+//useContext
+import { useContext } from "react";
+import { SomeContext } from "../../components/hooks/HookUseContext";
 
-//useContext 
-import { useContext } from 'react'
-import { SomeContext } from '../../components/hooks/HookUseContext'
+import HookUseEffect from "../../components/hooks/HookUseEffect";
+import HookUseReducer from "../../components/hooks/HookUseReducer";
+import HookUseState from "../../components/hooks/HookUseState";
+import HookUseRef from "../../components/hooks/HookUseRef";
 
 const Home = () => {
-  const {contextValue} = useContext(SomeContext)
+  const { contextValue } = useContext(SomeContext);
 
   return (
     <div>
@@ -17,9 +17,9 @@ const Home = () => {
       <HookUseEffect />
       <h2>UseContext</h2>
       <p>Valor do contexto: {contextValue}</p>
-      <hr />
+      <HookUseRef />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
